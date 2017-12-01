32c0314b38dfd62741c4cecd839fb657d8044fed
Barbeuk <tanguy.poret@hotmail.fr>
2017-12-01 17:58:25 +0100

    Check link integrity, force https, download file with same name without conflict

--- test.py
+++ test.py
@@ -1,14 +1,9 @@
+import os
 import requests
+import sys, getopt
+import re
 from requests import get
-from html.parser import HTMLParser
-from html.entities import name2codepoint
-
-r = requests.get('http://boards.4chan.org/w/thread/2043543')
-
-# print(r.headers['content-type'])
-# print(r.encoding)
-# après <div> de class fileText puis Data = file, premier <a> contient le liens vers image et champ data = nom fichier
-print(r.text)
+from FileIndexer import FourChanHtmlParser, FileIndex
 
 
 def download(url, file_name):
@@ -20,88 +15,47 @@ def download(url, file_name):
         file.write(response.content)
 
 
-class MyHTMLParser(HTMLParser):
-    def error(self, message):
-        pass
-
-    def handle_starttag(self, tag, attrs):
-        print("Start tag:", tag)
-        for attr in attrs:
-            print("     attr:", attr)
-
-    def handle_endtag(self, tag):
-        print("End tag  :", tag)
-
-    def handle_data(self, data):
-        print("Data     :", data)
-
-    def handle_comment(self, data):
-        print("Comment  :", data)
-
-    def handle_entityref(self, name):
-        c = chr(name2codepoint[name])
-        print("Named ent:", c)
-
-    def handle_charref(self, name):
-        if name.startswith('x'):
-            c = chr(int(name[1:], 16))
-        else:
-            c = chr(int(name))
-        print("Num ent  :", c)
-
-    def handle_decl(self, data):
-        print("Decl     :", data)
+def sanitise_url(url):
+    # https://boards.4chan.org/s/thread/17829010#p17837211
+    c = re.compile('^(https?)://boards.4chan.org/[a-z0-9]+/thread/.*$')
+    m = c.match(url)
+    if m is not None:
+        if m.group(1) == 'http':
+            url = 'https'+url[4:]
+        return url
+    else:
+        sys.exit(3)
 
 
-class FourChanHtmlParser(HTMLParser):
-    def __init__(self, *, convert_charrefs=True):
-        super().__init__(convert_charrefs=True)
-        self._step = 0
-        self.imgCount = 0
-        self.currentURL = ""
-        self.currentNAME = ""
+def main(argv):
+    mypath = ''
+    url = ''
+    try:
+        opts, args = getopt.getopt(argv, "hl:f:", ["link=", "folder="])
+    except getopt.GetoptError:
+        print('test.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            sys.exit()
+        if opt == '-l' or opt == '-link':
+            url = arg
+        if opt == '-f' or opt == '-folder':
+            mypath = arg
+    if not os.path.isdir(mypath):
+        os.makedirs(mypath)
+    index = FileIndex()
+    parser = FourChanHtmlParser(index)
+    url = sanitise_url(url)
+    r = requests.get(url)
+    parser.feed(r.text)
+    print()
+    print(parser.getImageCount())
+    index.resolve_duplicate()
 
-    def error(self, message):
-        pass
-
-    def handle_starttag(self, tag, attrs):
-        if tag == 'div' and self._step == 0:
-            print("Start tag:", tag)
-            for attr in attrs:
-                if attr[1] == 'fileText':
-                    print("     attr:", attr)
-                    self._step = 1
-        elif self._step == 1:
-            if tag == 'a':
-                for attr in attrs:
-                    if attr[0] == 'href':
-                        print("     attr:", attr)
-                        self.currentURL = attr[1]
-                        if self._step != 5:
-                            self._step = 2
-                    if attr[0] == 'title':
-                        # bypass the data part, title contains full name
-                        self._step = 5
-                        self.currentNAME = attr[1]
-                        print('lul', attr)
-                        break
-
-    def handle_data(self, data):
-        if self._step == 2:
-            print("Data     :", data)
-            self.currentNAME = data
-            print("URL:", self.currentURL[2:])
-            download("http://"+self.currentURL[2:], self.currentNAME)
-            self.imgCount = self.imgCount + 1
-            self._step = 0
-        if self._step == 5:
-            # Data bypasse, URL OK and NAME OK
-            print("URL:", self.currentURL[2:])
-            download("http://"+self.currentURL[2:], self.currentNAME)
-            self.imgCount = self.imgCount + 1
-            self._step = 0
+    for cle, valeur in index.get_files().items():
+        download("http://" + cle[2:], os.path.join(mypath, valeur))
+        # print(cle, os.path.join(mypath, valeur))
 
 
-parser = FourChanHtmlParser()
-parser.feed(r.text)
-print(parser.imgCount)
+main(sys.argv[1:])
