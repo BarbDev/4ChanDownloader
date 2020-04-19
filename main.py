399c6131e01636f25bab116abfce1cd4c69f5387
BarbDev <tanguy.poret@hotmail.fr>
2020-04-19 12:15:00 +0200

    sanitise_url() returns now a 'standardified' url without useless parts get_urls() does not return duplicate

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ def download(url, file_name) -> None:
 
 def sanitise_url(url: str) -> str:
     """
-    Make sure the URL correspond to 4chan and makes it HTTPS
+    Make sure the URL correspond to 4chan, trim useless parts and makes it HTTPS
     :param url: a string corresponding to a 4chan URL
     :return: a string URL with HTTPS
     """
     # https://boards.4chan.org/s/thread/17829010#p17837211
-    c = re.compile(r'^(https?)://boards.4chan(nel)?.org/[a-z0-9]+/thread/.*$')
+    c = re.compile(r'^((https?)://boards.4chan(nel)?.org/[a-z0-9]+/thread/\d+)(/.*$)?')
     m = c.match(url)
     if m is not None:
         if m.group(1) == 'http':
@@ -37,9 +37,15 @@ def sanitise_url(url: str) -> str:
 def get_urls(file_path):
     """
     Return a generator of URLs from a file
+    Eliminate duplicate in the generator (not in the file)
     """
+    previous_urls = set()
     with open(file_path) as file:
-        return (sanitise_url(url) for url in file.readlines())
+        for url in file.readlines():
+            sanitized_url = sanitise_url(url)
+            if sanitized_url not in previous_urls:
+                previous_urls.add(sanitized_url)
+                yield sanitized_url
 
 
 def thread_archived(chan_thread) -> bool:
